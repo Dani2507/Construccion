6e5b0935cd0fb56cd335bbf627d1d48b518ab0bc
TechnoKnightRX78 <77173798+TechnoKnightRX78@users.noreply.github.com>
2021-12-04 21:55:06 -0600

    Comandos
    Se creó la clase Comandos para Zuul

---
+++ src/modelo/Comandos.java
@@ -0,0 +1,27 @@
+package modelo;
+
+public class Comandos {
+    private String palabraComando;
+    private String segundaPalabraComando;
+    
+    public Comandos(String primerPalabra, String segundaPalabra) {
+        palabraComando = primerPalabra;
+        segundaPalabraComando = segundaPalabra;
+    }
+
+    public String getPalabraComando() {
+        return palabraComando;
+    }
+
+    public String getSegundaPalabraComando() {
+        return segundaPalabraComando;
+    }
+    
+    public boolean esDesconocido() {
+        return (palabraComando == null);
+    }
+    
+    public boolean tieneSegundaPalabra() {
+        return (segundaPalabraComando != null);
+    }
+}
