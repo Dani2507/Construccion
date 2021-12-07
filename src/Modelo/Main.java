12c32a2165484df17403138c5c3b0900a4f90647
José Fernando Chan Dzib <56355924+Jfeerchan23@users.noreply.github.com>
2021-12-07 01:32:31 -0500

    Librerías no usadas.
    Se quitaron importaciones que no se utilizaban.

--- src/Modelo/Main.java
+++ src/Modelo/Main.java
@@ -4,13 +4,6 @@
  */
 package Modelo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  *
  * @author 18K59LA
--- src/Modelo/ParsearArchivo.java
+++ src/Modelo/ParsearArchivo.java
@@ -23,25 +23,16 @@ public class ParsearArchivo {
         ArrayList<String[]> lista = new ArrayList<String[]>();
 
         try {
-            // Abrir el .csv en buffer de lectura
             bufferLectura = new BufferedReader(new FileReader(nombre));
-
-            // Leer una linea del archivo
             String linea = bufferLectura.readLine();
-
             while (linea != null) {
-                // Sepapar la linea leída con el separador definido previamente
-                String[] campos = linea.split(",");
-                //Agregamos los datos existentes del archivo CSV 
+                String[] campos = linea.split(",");  
                 lista.add(campos);
-
-                // Volver a leer otra línea del fichero
                 linea = bufferLectura.readLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            // Cierro el buffer de lectura
             if (bufferLectura != null) {
                 try {
                     bufferLectura.close();
